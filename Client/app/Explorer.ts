6877c5a9b5251e26606cc3770c3ec6f9ec55757a
kevin <kevin.ledy@gmail.com>
2016-05-10 16:34:50 +0200

    node module principalement

---
+++ Client/app/Explorer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "cloud-app",
+    templateUrl: "app/explorer.html",
+    //directives : [searchByNum] //classes a lier
+})
+export class Explorer {
+}
---
+++ Client/node_modules/protractor/typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/Q/Q.d.ts" />
+/// <reference path="browser/ambient/glob/glob.d.ts" />
+/// <reference path="browser/ambient/minimatch/minimatch.d.ts" />
+/// <reference path="browser/ambient/node/node.d.ts" />
