948b01a77357d21d64657d09369837bd8c20a9d8
phcollin <pierrehenri.collin@gmail.com>
2016-05-19 09:45:43 +0200

    authentification

--- Client/app/app.component.login.js
+++ Client/app/app.component.login.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/http', '@angular/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,26 +10,39 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var http_1, core_1;
     var Login;
     return {
         setters:[
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
             Login = (function () {
-                function Login() {
+                function Login(http) {
+                    this.http = http;
                 }
                 Login.prototype.connectDropbox = function () {
-                    window.location.href = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/cloudAuthorize?cloud=db&callbackUri=http://localhost:8080/WSCloudUnifierService/cloudUnifier';
+                    window.location.href = this.url;
+                };
+                Login.prototype.getURL = function () {
+                    var _this = this;
+                    this.http.get('http://localhost:8080/WSCloudUnifierService/cloudUnifier/cloudAuthorize?cloud=db&callbackUri=http://localhost:8080/WSCloudUnifierService/cloudUnifier')
+                        .map(function (res) { return res.text(); })
+                        .subscribe(function (data) { return _this.url = data; }, function (err) { return _this.logError(err); }, function () { return _this.connectDropbox(); });
+                };
+                Login.prototype.logError = function (err) {
+                    console.error('ERROR !');
                 };
                 Login = __decorate([
                     core_1.Component({
                         selector: 'my_login',
-                        template: "\n  <header>\n    <h1 class=\"title\">Connection</h1>\n  </header>\n  <button class=\"btn btn-primary\" (click)=\"connectDropbox()\">Dropbox authentification</button>\n  "
+                        template: "\n  <header>\n    <h1 class=\"title\">Connection</h1>\n  </header>\n  <button class=\"btn btn-primary\" (click)=\"getURL()\">DROPBOX</button>\n  <br><br><br>\n  "
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [http_1.Http])
                 ], Login);
                 return Login;
             }());
--- Client/app/app.component.login.ts
+++ Client/app/app.component.login.ts
@@ -1,5 +1,7 @@
-import {Http, Headers} from 'angular2/http';
+import {Http, Headers, HTTP_PROVIDERS, HTTP_BINDINGS} from '@angular/http';
 import {Component} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES} from '@angular/common';
+
 
 @Component({
 	selector: 'my_login',
@@ -7,18 +9,34 @@ import {Component} from '@angular/core';
   <header>
     <h1 class="title">Connection</h1>
   </header>
-  <button class="btn btn-primary" (click)="connectDropbox()">Dropbox authentification</button>
+  <button class="btn btn-primary" (click)="getURL()">DROPBOX</button>
+  <br><br><br>
   `
 })
 
 export class Login{
 
-	constructor() {
+  url: string;
+
+	constructor(public http:Http) {
 
 	}
 
   connectDropbox() {
-    window.location.href = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/cloudAuthorize?cloud=db&callbackUri=http://localhost:8080/WSCloudUnifierService/cloudUnifier';
+    window.location.href = this.url;
   }
 
+  getURL() {
+    this.http.get('http://localhost:8080/WSCloudUnifierService/cloudUnifier/cloudAuthorize?cloud=db&callbackUri=http://localhost:8080/WSCloudUnifierService/cloudUnifier')
+      .map(res => res.text())
+      .subscribe(
+      data => this.url = data,
+      err => this.logError(err),
+      () => this.connectDropbox()
+      );
+  }
+
+  logError(err){
+    console.error('ERROR !');
+  }
 }
