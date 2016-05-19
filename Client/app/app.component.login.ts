dc002e4e44ae6b6b9ca38c1a8c4fbb0e8123b17b
phcollin <pierrehenri.collin@gmail.com>
2016-05-19 17:27:36 +0200

    login

--- Client/app/app.component.login.ts
+++ Client/app/app.component.login.ts
@@ -9,8 +9,9 @@ import {Connected} from './app.connected.service'
   <h1 class="title">Connection</h1>
   </header>
   <span id="buttons">
-  <button style="height:50px;width:100px"class="btn btn-primary" (click)="connectDropbox()">DROPBOX</button>
-  <button style="height:50px;width:100px" class="btn btn-primary" (click)="connectDropbox()">GOOGLE DRIVE</button>
+  <button style="height:50px;width:200px"class="btn btn-primary" (click)="connectDropbox()">DROPBOX</button>
+  <button style="height:50px;width:200px" class="btn btn-primary" (click)="connectDropbox()">GOOGLE DRIVE</button>
+  <button style="height:50px;width:200px" class="btn btn-primary" (click)="getConnected()">KRIBOUILLE IS MAGIC</button>
   </span>
   <br><br><br>
   `
@@ -19,43 +20,48 @@ import {Connected} from './app.connected.service'
 export class Login{
 
   url: string;
+  url2: string;
   urlConnect: string;
-  headers: Headers;
 
   constructor(public http: Http, private connect : Connected) {
-    this.headers = new Headers();
-      this.headers.append('Content-Type', 'application/json');
-      this.headers.append('Access-Control-Allow-Origin', '*');
   }
 
 /**
  * Se connecter à la fenêtre d'authentification de Dropbox
  */
   connectDropbox() {
-    this.url = 'https://www.dropbox.com/1/oauth2/authorize?client_id=wl5n5wq11bvcnst&response_type=code&redirect_uri=http://localhost:3000/allFiles';
-    this.http.get(this.url, this.headers)
+    this.url = 'https://www.dropbox.com/1/oauth2/authorize?client_id=wl5n5wq11bvcnst&response_type=code&redirect_uri=http://localhost:8080/WSCloudUnifierService/cloudUnifier/authenticate?cloud=db';
+    this.http.get(this.url)
     .map(res => res.text())
     .subscribe(  
-      data => console.log('Connection ...'),
+    data => {
+      this.connect.setConnect(true),
+      console.log('Connection qui passe à true')
+    },
       err => this.logError(err),
-      () => window.location.href = this.url
-      );
-  }
+      () => {
+      window.location.href = this.url;
+    }      
+         );
+     }
 
 /**
  * Vérifie que le client est connecté
  */
-  // isconnected(){
-  //   this.urlConnect = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/isConnected?cloud=db';
-  //   this.http.get(this.urlConnect)
-  //   .map(res => res.json())
-  //   .subscribe
-  //   (
-  //     data => this.connect.setConnect(data.isConnected),
-  //     err => this.logError(err),
-  //     () => console.log("Connected")
-  //     );
-  // }
+  getConnected(){
+    this.urlConnect = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/isConnected?cloud=db';
+    this.http.get(this.urlConnect)
+    .map(res => res.json())
+    .subscribe
+    (
+    data => {
+      this.connect.setConnect(data.isConnected),
+      console.log("Suis-je connecté ?" + data.isConnected)
+    },
+      err => this.logError(err),
+      () => console.log("Connected")
+      );
+  }
 
   logError(err){
     console.error('ERROR !');
