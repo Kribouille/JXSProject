f7370b7baedfdc1ad53db84e295ba80f5c933ed0
Tha1n <alx.quettier@gmail.com>
2016-05-19 10:13:17 +0200

    Correct commit

--- Server/WSCloudUnifierService/src/main/java/net/jxsproject/ws/service/cloudUnifier/WSCloudUnifierService.java
+++ Server/WSCloudUnifierService/src/main/java/net/jxsproject/ws/service/cloudUnifier/WSCloudUnifierService.java
@@ -37,9 +37,9 @@ public class WSCloudUnifierService {
 	@Path("authenticate")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response authenticate(@QueryParam("cloud") String cloud, @QueryParam("code") String code, @QueryParam("callbackUri") String callbackUri) {
-        if(cloud.equals("db"))
+		if(cloud.equals("db"))
 			return this.dropbox.authenticate(code, callbackUri);
-        else if (cloud.equals("drive"))
+		else if (cloud.equals("drive"))
 			return this.drive.authenticate(code, callbackUri);
 		else
 			return null;
@@ -49,9 +49,9 @@ public class WSCloudUnifierService {
 	@Path("getFDetails")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getFileDetails(@QueryParam("cloud") String cloud, @QueryParam("path") String path) {
-        if(cloud.equals("db"))
+		if(cloud.equals("db"))
 			return this.dropbox.getFileDetails(path);
-        else if (cloud.equals("drive"))
+		else if (cloud.equals("drive"))
 			return this.drive.getFileDetails(path);
 		else
 			return null;
@@ -89,6 +89,18 @@ public class WSCloudUnifierService {
 			return this.dropbox.moveFile(pathFrom, pathTo);
 		else if (cloud.equals("drive"))
 			return this.drive.moveFile(pathFrom, pathTo);
+		else
+			return null;
+	}
+
+	@GET
+	@Path("addFile")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addFile(@QueryParam("cloud") String cloud, @QueryParam("pathFrom") String pathFrom, @QueryParam("pathTo") String pathTo) {
+		if(cloud.equals("db"))
+			return this.dropbox.addFile(pathFrom, pathTo);
+		else if (cloud.equals("drive"))
+			return this.drive.addFile(pathFrom, pathTo);
 		else
 			return null;
 	}
