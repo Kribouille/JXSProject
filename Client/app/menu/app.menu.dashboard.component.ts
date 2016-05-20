bda8102653de95514f581596d88a6cfa019b52b7
phcollin <pierrehenri.collin@gmail.com>
2016-05-20 14:45:31 +0200

    color mouseover tab

--- Client/app/menu/app.menu.dashboard.component.ts
+++ Client/app/menu/app.menu.dashboard.component.ts
@@ -4,43 +4,73 @@ import {CORE_DIRECTIVES, FORM_DIRECTIVES} from '@angular/common';
 @Component({
   selector:"dashboard",
   template:`
+  	<p>DROPBOX</p>
  	{{url}}<br>
- 	{{name}}<br>
- 	{{surname}}<br>
- 	{{mail}}<br>
- 	{{country}}<br>
- 	{{spaceFree}}<br>
- 	{{spaceAvailable}}<br>
+ 	{{nameDB}}<br>
+ 	{{surnameDB}}<br>
+ 	{{mailDB}}<br>
+ 	{{countryDB}}<br>
+ 	{{spaceUsedDB}}<br>
+ 	{{spaceAvailableDB}}<br>
+ 	<p>GOOGLE DRIVE<p>
+ 	{{nameDR}}<br>
+ 	{{mailDR}}<br>
+ 	{{countryDR}}<br>
+ 	{{spaceUsedDR}}<br>
+ 	{{spaceTotalDR}}<br>
   `
 })
 export class DashBoardComponent{
 	url: string;
-	name: string;
-	surname: string;
-	mail: string;
-	country: string;
-	spaceFree: number;
-	spaceAvailable: number;
+	nameDB: string;
+	surnameDB: string;
+	mailDB: string;
+	countryDB: string;
+	spaceUsedDB: number;
+	spaceAvailableDB: number;
+	nameDR: string;
+	mailDR: string;
+	countryDR: string;
+	spaceUsedDR: number;
+	spaceTotalDR: number;
 
 	constructor(private http:Http){
-		this.getUserInfo();
+		this.getUserInfoDropbox();
+		this.getUserInfoDrive();
+
 	}
 
-	getUserInfo() {
+	getUserInfoDropbox() {
 		this.url = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/getUDetails?cloud=db';
 		this.http.get(this.url)
 			.map(res => res.json())
 			.subscribe(
 			data => {
-				this.name = data.name_details.familiar_name;
-				this.surname = data.name_details.surname;
-				this.mail = data.email;
-				this.spaceFree = data.quota_info.normal;
-				this.spaceAvailable = data.quota_info.quota;
-				this.country = data.country;
+				this.nameDB = data.name_details.familiar_name;
+				this.surnameDB = data.name_details.surname;
+				this.mailDB = data.email;
+				this.spaceUsedDB = data.quota_info.normal;
+				this.spaceAvailableDB = data.quota_info.quota;
+				this.countryDB = data.country;
 			},
 			err => this.logError(err),
 			() => {}
+			);
+	}
+	getUserInfoDrive(){
+		this.url = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/getUDetails?cloud=drive';
+		this.http.get(this.url)
+			.map(res => res.json())
+			.subscribe(
+			data => {
+				this.nameDR = data.name;
+				this.mailDR = data.emailAddress;
+				this.spaceUsedDR = data.quotaBytesUsed;
+				this.spaceTotalDR = data.quotaBytesTotal;
+				this.countryDR = data.languageCode;
+			},
+			err => this.logError(err),
+			() => { }
 			);
 	}
 
