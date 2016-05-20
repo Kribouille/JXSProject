49e62550b8d755d86474ecf6eebc8aa72a7d8189
kevin <kevin.ledy@gmail.com>
2016-05-20 11:23:30 +0200

    parcours del'arborescence

--- Client/app/menu/app.menu.allFiles.component.ts
+++ Client/app/menu/app.menu.allFiles.component.ts
@@ -20,20 +20,24 @@ export class AllFilesComponent{
   public files;
   private nbFolders = 0;
   private _selected : FileFolder;
+  private _currentPath : string;
 
   constructor(public http: Http){
     this.folders = new Array<FileFolder>();
     this.http=http;
-    this.getFiles();
+    this._currentPath = '/';
+    this.getFiles('/');
 
 }
-    getFiles(){
+    getFiles(path:string){
       //dropbox for now
-      return this.getFilesDropbox();
+      return this.getFilesDropbox(path);
     }
 
-    getFilesDropbox() {
-        this.http.get('http://localhost:8080/WSCloudUnifierService/cloudUnifier/getTree?cloud=db&path=/')
+    getFilesDropbox(path: string) {
+        this._currentPath=path;
+        console.log("path :" + this._currentPath);
+        this.http.get('http://localhost:8080/WSCloudUnifierService/cloudUnifier/getTree?cloud=db&path='+path)
         .map(res => res.json())
         .subscribe(
           data => this.files = data,
@@ -43,27 +47,29 @@ export class AllFilesComponent{
     }
 
     getFilesFromDropbox(){
-       // console.log(this.files);
         var details = this.files.files;
+        this.folders = new Array<FileFolder>();
         for(var i = 0; i < details.length; i++){
             var name = details[i].path;
-            this.folders.push(new FileFolder(name, this.http));
+            this.folders.push(new FileFolder(name, this.http, this._currentPath));
             this.nbFolders++;
         }
-        console.log(this.folders[0]);
+        console.log(this);
     }
 
-    onSelect(f : FileFolder) { this._selected = f; f.getInfos();}
+    onSelectFolder (f: FileFolder){ this._selected = f; this.getFiles(f._name);}
+    onSelectInfo(f : FileFolder) { this._selected = f; f.requestInfos();}
 
     logError(err) {
         console.error('ERROR get all files/folders ' + err);
     }
+
 }
 
 
 
 class FileFolder{
-    _name: String;
+    _name: string;
     _isFolder : boolean; // true si folder false si file
     _toDisplay : boolean = true;
 
@@ -71,19 +77,24 @@ class FileFolder{
     _toDisplayAndIsFile : boolean;
     private _informations;
 
-    constructor(public name : String, public http: Http){
+    constructor(public name : string, public http: Http, path:string){
 
-        //inititalisation du type
+        //inititalisation du name
         this._name = name;
-        if(this._name.indexOf('/') > -1){  //on n'affiche pas
+
+
+        //initialisation pour l'affichage, et le type
+        var fileName = this._name;
+        fileName = fileName.substr(path.length+1, fileName.length);
+        if(fileName.indexOf('/') > -1){  //on n'affiche pas
 
           this._toDisplay = false;
-          if (this._name.indexOf('.')> -1) { this._isFolder = false;} //le fichier est dans un sous-dossier
+          if (fileName.indexOf('.')> -1) { this._isFolder = false;} //le fichier est dans un sous-dossier
           else { this._isFolder = true; }// sous-dossier
 
         }
         else{//on affiche
-          if(this._name.indexOf('.')> -1) {   this._isFolder = false;} // cas ou le fichier est a la racine
+          if(fileName.indexOf('.')> -1) {   this._isFolder = false;} // cas ou le fichier est a la racine
           else{ this._isFolder = true;}//dossier dans la racine
 
         }
@@ -94,11 +105,19 @@ class FileFolder{
         //console.log("name : " + this._name +"is Dir :" + informations[0]);
     }
 
-    private setInfos(infos){
+    setInfos(infos){
       this._informations = infos;
     }
     getInfos(){
-      var url = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/getFDetails?cloud=db&path=' + this._name;
+      return this._informations;
+  }
+
+    requestInfos(){
+      var fileName = this._name;
+      //fileName = this.replaceAll(fileName, " ", "%20");
+
+      var url = 'http://localhost:8080/WSCloudUnifierService/cloudUnifier/getFDetails?cloud=db&path=' + fileName;
+      console.log(url);
       this.http.get(url)
       .map(res => res.json())
       .subscribe(
@@ -108,6 +127,22 @@ class FileFolder{
       console.log("infos : " + this._informations);
       return this._informations;
     }
+
+
+
+    private replaceAll(strFrom : string, c :string, sub:string){
+      var res = "";
+      for (let i = 0; i< strFrom.length; i++){
+          if(strFrom.charAt(i) != c){
+            res = res + strFrom.charAt(i);
+          }
+          else{
+            res += sub;
+          }
+      }
+      return res;
+    }
+
 
 
     logError(err) {
